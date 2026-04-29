9a6fa580013cca802a9e96b6725f4564ca69af11
Bogumił Harnik <bogumilharnik@gmail.com>
2026-04-29 10:29:18 +0200

    Modify file

--- export_files_to_txt_file.sh
+++ export_files_to_txt_file.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-
-( find . -type d -print0 | sort -z | while IFS= read -r -d '' d; do     echo "===== KATALOG: $d =====";     find "$d" -maxdepth 1 -type f ! -name 'wynik.txt' -print0 | sort -z | while IFS= read -r -d '' f; do       echo "----- PLIK: $f -----";       cat "$f";       echo;     done;     echo;   done ) > wynik.txt
+find . -type d -print0 | sort -z | while IFS= read -r -d '' d; do     printf -- "===== KATALOG: %s =====\n" "$d";     find "$d" -maxdepth 1 -type f       ! -name 'wynik.txt'       ! -name 'vault.pass'       ! -path './.git/*'       -print0 | sort -z | while IFS= read -r -d '' f; do         printf -- "----- PLIK: %s -----\n" "$f";         cat "$f";         printf -- "\n";     done;     printf -- "\n";   done ) > wynik.txt
