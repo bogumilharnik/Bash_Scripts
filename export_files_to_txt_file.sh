d68aeca678194848b42a65c15abcb7e7b8284b13
bogumilharnik <117505733+bogumilharnik@users.noreply.github.com>
2026-07-14 12:58:30 +0200

    Update export_files_to_txt_file.sh

--- export_files_to_txt_file.sh
+++ export_files_to_txt_file.sh
@@ -1,1 +1,13 @@
-find . -type d -print0 | sort -z | while IFS= read -r -d '' d; do     printf -- "===== KATALOG: %s =====\n" "$d";     find "$d" -maxdepth 1 -type f       ! -name 'wynik.txt'       ! -name 'vault.pass'       ! -path './.git/*'       -print0 | sort -z | while IFS= read -r -d '' f; do         printf -- "----- PLIK: %s -----\n" "$f";         cat "$f";         printf -- "\n";     done;     printf -- "\n";   done ) > wynik.txt
+( find . -type d -print0 | sort -z | while IFS= read -r -d '' d; do
+    printf -- "===== KATALOG: %s =====\n" "$d"
+    find "$d" -maxdepth 1 -type f \
+      ! -name 'wynik.txt' \
+      ! -name 'vault.pass' \
+      ! -path './.git/*' \
+      -print0 | sort -z | while IFS= read -r -d '' f; do
+        printf -- "----- PLIK: %s -----\n" "$f"
+        cat "$f"
+        printf -- "\n"
+    done
+    printf -- "\n"
+  done ) > wynik.txt
